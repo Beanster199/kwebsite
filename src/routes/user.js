d07fb11c6b018c608d0324638bd41c710da1aa6f
Mariano Moretti <43387209+Beanster199@users.noreply.github.com>
2018-12-26 17:34:17 -0300

    Fixed timeago.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,19 +4,25 @@ const connection = require('../config/dbConnection');
 
 app.get('/:nameId', async (req, res) => {
     if (req.params.nameId) {
-        const query = 'SELECT kwe_lastlogin as lastlogin,kwe_name as name FROM kwebsite_users where kwe_name = "Resucting"';
+        const query = 'SELECT kwe_lastlogin as lastlogin,kwe_name as name FROM kwebsite_users where kwe_name = "Benster_199"';
+        const friends = await connection.query('select * from kwebsite_friends')
         const profile = await connection.query(query)
         console.log(profile)
             if (profile.length > 0) {
-                res.render('../views/u/user_profile.hbs', {
-                    user: profile
-                })
-            } else {
-                res.render('status/user_404', {
-                    error: req.params.nameId
-                });
-        };
-        return console.log(req.params.nameId);
+                console.log(profile )
+                console.log(profile[0].lastlogin)
+                res.render('../views/u/user_profile.hbs' , {
+                    user: profile,
+                    friends: friends
+                  }
+                )
+            } 
+//            else {
+//                res.render('status/user_404', {
+//                    error: req.params.nameId
+//               });
+//        };
+//        return console.log(req.params.nameId);
     } else {
         res.redirect('/')
     }
