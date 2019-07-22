92363bc258d33f47172334eb35bdd089db7de403
Marian-CGE <mmoretticom@gmail.com>
2019-07-22 20:53:01 -0300

    fixed auth

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -15,7 +15,9 @@ auth.use('token-login', new Strategy({
   async (req,token,ip, done) => {
     let date_ago = new Date();
     date_ago.setMinutes(date_ago.getMinutes() - 1024)
-    const _token = await connection.query('SELECT kut_id,kut_uuid as uuid,name FROM kwebsite_users_tokens INNER JOIN ksystem_playerdata ksp ON ksp.uuid = kut_uuid WHERE kut_id = ? and kut_ip = ? and kut_date between ? and ?', [token,ip,date_ago.toLocaleString(),new Date().toLocaleString()])
+    console.log(`SELECT token,kut.uuid,name FROM kwebsite_users_tokens kut INNER JOIN ksystem_playerdata ksp ON ksp.uuid = kut.uuid WHERE token = ${token} and kut.address = ${ip} and servertime between ${date_ago.toLocaleString()} and ${new Date().toLocaleString()}`)
+    const _token = await connection.query('SELECT token,kut.uuid,name FROM kwebsite_users_tokens kut INNER JOIN ksystem_playerdata ksp ON ksp.uuid = kut.uuid WHERE token = ? and kut.address = ? and servertime between ? and ?', [token,ip,date_ago.toLocaleString(),new Date().toLocaleString()]);
+    console.log(_token)
     if(!_token[0] || !_token){
       console.log('Token invalido o usuario no encontrado. Retornando user:false;')
       return done(null,false)
