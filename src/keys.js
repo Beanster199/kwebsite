f94e99e37ac4510cc999a80faf19a3c406d95215
Main Desktop <mmoretticom@gmail.com>
2019-08-09 19:19:46 -0300

    i need this not that

--- src/keys.js
+++ src/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     database: {
         connectionLimit: 10,
-        host: '34.74.110.173',
+        host: '34.83.53.62',
         user: 'benster',
         password: 'o86FZYGBmN%#$lo8djF#&$#$%#$&#',
         database: 'ksystem'
