2360d7dbefb94f4d28c55cf65dbee7c0f2c491c8
Main Desktop <mmoretticom@gmail.com>
2019-09-08 04:29:07 -0300

    fix it

--- src/routes/tools.js
+++ src/routes/tools.js
@@ -4,4 +4,8 @@ app.get('/clicks', async (req,res) => {
     res.render('../views/clicks.hbs');
 });
 
+app.get('/rules', async(req,res) => {
+    res.render('../views/rules.hbs');
+});
+
 module.exports = app;
