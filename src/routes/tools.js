16255a46895df53fba26bd15270e68298758ae48
Main Desktop <mmoretticom@gmail.com>
2019-09-28 04:47:23 -0300

    push

--- src/routes/tools.js
+++ src/routes/tools.js
@@ -1,4 +1,5 @@
 const app = require('express').Router();
+const connection = require('../config/dbConnection');
 
 app.get('/clicks', async (req,res) => {
     res.render('../views/clicks.hbs');
@@ -10,6 +11,20 @@ app.get('/rules', async(req,res) => {
 
 app.get('/countdown', async (req,res) => {
 
+});
+
+app.get('/map-:map', async (req,res) => {
+    if(!req.params.map){
+        return res.redirect('/');
+    }
+    req.params.map = `map-${req.params.map}`
+    const _mapkit = await connection.query('select * from kwebsite_mapkit where map_id = ?',req.params.map);
+    if(!_mapkit[0]){
+        return res.redirect('/');
+    }
+    console.log(_mapkit)
+    _mapkit[0].maps = _mapkit;
+    res.render('../views/map-kit.hbs', {mapkit: _mapkit[0]});
 });
 
 module.exports = app;
